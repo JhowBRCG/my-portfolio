7c1be6652000851873f2e6dd1967a0dc47ae32d4
Jhonathan <guarino90@gmail.com>
2024-09-27 22:29:52 -0300

    remove class dark:border-x and replace it with boder-x

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="relative mx-[22px] border-line-light px-[10px] py-[16px] dark:border-x dark:border-line-dark">
+    <header className="relative mx-[22px] border-x border-line-light px-[10px] py-[16px] dark:border-line-dark">
       <h2 className="text-xl">Welcome</h2>
     </header>
   );
