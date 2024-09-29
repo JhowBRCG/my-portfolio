8a7880b6b94d4065b63413506915899d5406ec85
Jhonathan <guarino90@gmail.com>
2024-09-29 19:49:58 -0300

    set font size for different screens sizes

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -12,21 +12,21 @@ export default function Header() {
   const closeMenu = () => setMenu(false);
 
   return (
-    <header className="relative border-x border-line-light px-[10px] py-[16px] dark:border-line-dark">
+    <header className="relative border-x border-line-light px-[10px] py-[16px] dark:border-line-dark 2xl:pl-[40px]">
       <div className="flex items-center justify-between">
-        <h2 className="text-xl">Welcome</h2>
+        <h2 className="text-xl 2xl:text-3xl">Welcome</h2>
         <ToggleTheme className="absolute right-28 top-0 md:right-80" />
         <ul className="hidden md:flex">
-          <li className="relative px-[20px] text-lg">
+          <li className="relative px-[20px] text-lg 2xl:text-2xl">
             <a href="">Contact</a>
-            <span className="absolute -top-[16px] right-0 h-[60px] w-[1px] bg-line-light dark:bg-line-dark"></span>
-            <span className="absolute -top-[16px] left-0 h-[60px] w-[1px] bg-line-light dark:bg-line-dark"></span>
+            <span className="absolute -top-[16px] right-0 h-[60px] w-[1px] bg-line-light dark:bg-line-dark 2xl:h-[67px]"></span>
+            <span className="absolute -top-[16px] left-0 h-[60px] w-[1px] bg-line-light dark:bg-line-dark 2xl:h-[67px]"></span>
           </li>
-          <li className="relative px-[20px] text-lg">
+          <li className="relative px-[20px] text-lg 2xl:text-2xl">
             <a href="">GitHub</a>
-            <span className="absolute -top-[16px] right-0 h-[60px] w-[1px] bg-line-light dark:bg-line-dark"></span>
+            <span className="absolute -top-[16px] right-0 h-[60px] w-[1px] bg-line-light dark:bg-line-dark 2xl:h-[67px]"></span>
           </li>
-          <li className="px-[20px] text-lg">
+          <li className="px-[20px] text-lg 2xl:text-2xl">
             <a href="">CV</a>
           </li>
         </ul>
