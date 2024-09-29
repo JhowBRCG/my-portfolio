bcda653a1b8e4e90f8ee0e66ca788905b0b50b15
Jhonathan <guarino90@gmail.com>
2024-09-28 23:42:42 -0300

    add MobileMenu inside of Header component, and add logic to open and close the menu

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import ToggleTheme from "../ToggleTheme";
+import MobileMenu from "../MobileMenu";
+import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
 
 export default function Header() {
+  const [menu, setMenu] = useState(false);
+
+  const openMenu = () => setMenu(true);
+  const closeMenu = () => setMenu(false);
+
   return (
     <header className="relative mx-[22px] border-x border-line-light px-[10px] py-[16px] dark:border-line-dark">
-      <div className="flex items-center">
+      <div className="flex items-center justify-between">
         <h2 className="text-xl">Welcome</h2>
         <ToggleTheme className="absolute right-28 top-0" />
+        <button onClick={openMenu}>
+          <CiMenuFries className="text-3xl" />
+        </button>
+        <MobileMenu closeMenu={closeMenu} menu={menu} />
       </div>
     </header>
   );
