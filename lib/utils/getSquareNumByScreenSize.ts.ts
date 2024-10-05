78b8a3a624fcfb4277e51b6160775a8a431968f2
Jhonathan <guarino90@gmail.com>
2024-10-05 18:05:21 -0300

    create getSquareNumByScreenSize utilitary function

---
+++ lib/utils/getSquareNumByScreenSize.ts.ts
@@ -0,0 +1,12 @@
+export function getSquareNumByScreenSize(
+  isDesktop: boolean,
+  isLaptop: boolean,
+  isTablet: boolean,
+  isMobileL: boolean,
+) {
+  if (isDesktop) return 30;
+  if (isLaptop) return 20;
+  if (isTablet) return 15;
+  if (isMobileL) return 11;
+  return 10;
+}
