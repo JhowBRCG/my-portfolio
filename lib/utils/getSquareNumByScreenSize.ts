ac594e22f44c71cffd10132af2f1304d80354a45
Jhonathan <guarino90@gmail.com>
2024-10-10 20:55:37 -0300

    change numbers of squares for each screen size

--- lib/utils/getSquareNumByScreenSize.ts
+++ lib/utils/getSquareNumByScreenSize.ts
@@ -4,8 +4,8 @@ export function getSquareNumByScreenSize(
   isTablet: boolean,
   isMobileL: boolean,
 ) {
-  if (isDesktop) return 30;
-  if (isLaptop) return 20;
+  if (isDesktop) return 22;
+  if (isLaptop) return 15;
   if (isTablet) return 22;
   if (isMobileL) return 11;
   return 10;
