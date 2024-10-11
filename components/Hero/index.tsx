854b55d0f0c76255eb5efcb74d75c41004596be6
Jhonathan <guarino90@gmail.com>
2024-10-10 21:48:48 -0300

    change text size for medium devices screen size

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -34,24 +34,24 @@ export default function Hero() {
       </div>
       <div className="flex flex-col items-center justify-center text-center">
         <div className="flex flex-col gap-[8px]">
-          <h1 className="animate-fade-left text-[20px] delay-100">
+          <h1 className="animate-fade-left text-[20px] delay-100 md:text-2xl">
             FrontEnd Developer
           </h1>
-          <h2 className="animate-fade-right text-[24px] delay-200">
+          <h2 className="animate-fade-right text-[24px] delay-200 md:text-3xl">
             HELLO, I&apos;M JHONATHAN
           </h2>
         </div>
         <div className="mt-[30px] flex items-center gap-[28px]">
-          <p className="animate-fade rounded-full border border-dashed border-black px-[14px] py-[7px] text-[12px] animate-delay-500 dark:border-white">
+          <p className="text animate-fade rounded-full border border-dashed border-black px-[14px] py-[7px] text-[12px] animate-delay-500 dark:border-white md:text-[14px]">
             minimalist design
           </p>
-          <p className="animate-fade rounded-full border border-dashed border-black px-[14px] py-[7px] text-[12px] animate-delay-500 dark:border-white">
+          <p className="animate-fade rounded-full border border-dashed border-black px-[14px] py-[7px] text-[12px] animate-delay-500 dark:border-white md:text-[16px]">
             web applications
           </p>
         </div>
         <a
           href="#"
-          className="mt-[73px] animate-jump-in border-[0.5px] border-black px-[29px] py-[11px] text-[14px] animate-delay-1000 dark:border-white"
+          className="mt-[73px] animate-jump-in border-[0.5px] border-black px-[29px] py-[11px] text-[14px] animate-delay-1000 dark:border-white md:text-[18px]"
         >
           CONTACT-ME
         </a>
