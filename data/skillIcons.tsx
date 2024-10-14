533d85c23de6f1bca44c81754b1ac5445aaf3bdc
Jhonathan <guarino90@gmail.com>
2024-10-14 15:39:07 -0300

    add skillIcons

---
+++ data/skillIcons.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { Icon } from "@iconify/react";
+
+export const skillIcons = [
+  {
+    name: "HTML",
+    icon: <Icon icon="bxl:html5" />,
+  },
+  {
+    name: "Css",
+    icon: <Icon icon="bxl:css3" />,
+  },
+  {
+    name: "JavaScript",
+    icon: <Icon icon="cib:js" />,
+  },
+  {
+    name: "React",
+    icon: <Icon icon="nonicons:react-16" />,
+  },
+  {
+    name: "TypeScript",
+    icon: <Icon icon="teenyicons:typescript-solid" />,
+  },
+  {
+    name: "Sass",
+    icon: <Icon icon="fa6-brands:sass" />,
+  },
+  {
+    name: "Styled components",
+    icon: <Icon icon="file-icons:styledcomponents" />,
+  },
+  {
+    name: "Tailwind",
+    icon: <Icon icon="bxl:tailwind-css" />,
+  },
+  {
+    name: "Git",
+    icon: <Icon icon="bi:git" />,
+  },
+  {
+    name: "Next.js",
+    icon: <Icon icon="akar-icons:nextjs-fill" />,
+  },
+];
