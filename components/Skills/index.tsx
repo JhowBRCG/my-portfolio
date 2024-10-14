bbbd4909a83ec69e27fe4fbd0499f86c3f831f0b
Jhonathan <guarino90@gmail.com>
2024-10-14 16:43:11 -0300

    using useEmblaCarousel hook to create a carrousel feature

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import useEmblaCarousel from "embla-carousel-react";
+import { skillIcons } from "@/data/skillIcons";
+
 export default function Skills() {
-  return <section>Skills</section>;
+  const [emblaRef] = useEmblaCarousel({
+    dragFree: true,
+    loop: true,
+    align: "start",
+  });
+  return (
+    <section
+      className="overflow-hidden border-y border-line-light dark:border-line-dark"
+      ref={emblaRef}
+    >
+      <ul className="flex items-center divide-x-[1px] divide-line-light dark:divide-line-dark">
+        {skillIcons.map((icon, i) => (
+          <li
+            key={i}
+            className="flex flex-[0_0_50px] justify-center px-[9px] py-[18px] text-[38px] first:border-l first:dark:border-line-dark"
+          >
+            {icon.icon}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
