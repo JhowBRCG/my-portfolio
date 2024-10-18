4f3cd66416bf3cec5519f1be689c57e3c8a34142
Jhonathan <guarino90@gmail.com>
2024-10-18 20:51:44 -0300

    create SubTitle component

---
+++ components/SubTitle/index.tsx
@@ -0,0 +1,18 @@
+type SubTitleProps = {
+  text: string;
+  textCenter: boolean;
+};
+
+export default function SubTitle({ text, textCenter }: SubTitleProps) {
+  const isTextCenter = textCenter && (
+    <div className="h-[1px] flex-grow bg-line-light dark:bg-line-dark" />
+  );
+
+  return (
+    <div className="flex items-center gap-[6px]">
+      {isTextCenter}
+      <h2 className="text-[24px]">{text}</h2>
+      <div className="h-[1px] flex-grow bg-line-light dark:bg-line-dark" />
+    </div>
+  );
+}
