0d9617e9a268b727033dca4179885c49c2e4f12f
Jhonathan <guarino90@gmail.com>
2024-10-18 19:28:34 -0300

    add About component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
+import About from "@/components/About";
 
 export default function page() {
   return (
     <main>
       <Hero />
       <Skills />
+      <About />
     </main>
   );
 }
