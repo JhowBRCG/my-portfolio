1bad089cde717236ab70bf6c16d55ef5fdedfdd6
Jhonathan <guarino90@gmail.com>
2024-10-19 01:02:09 -0300

    add margin

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -2,7 +2,7 @@ import SubTitle from "../SubTitle";
 
 export default function About() {
   return (
-    <section>
+    <section className="mx-[22px] mt-[51px]">
       <SubTitle text="Know me a litte bit" textCenter={false} />
     </section>
   );
