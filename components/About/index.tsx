0bc9bd1608d3ad5b181f1a0be45a48a94d04dead
Jhonathan <guarino90@gmail.com>
2024-10-19 01:44:36 -0300

    create a <div> container for the background image

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -4,6 +4,7 @@ export default function About() {
   return (
     <section className="mx-[22px] mt-[51px]">
       <SubTitle text="Know me a litte bit" textCenter={false} />
+      <div className="dark:bg-about-mobile-dark bg-about-mobile-light mt-[25px] min-h-[624px] w-full border border-line-light bg-contain bg-bottom bg-no-repeat dark:border-line-dark"></div>
     </section>
   );
 }
