4302818bb6b512a89739dbde8862addbf13940c1
Jhonathan <guarino90@gmail.com>
2024-10-20 23:01:20 -0300

    reorder classes position

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -12,31 +12,31 @@ export default function About() {
       <SubTitle text="Know me a litte bit" textCenter={false} />
       <div className="mt-[25px] min-h-[624px] w-full border border-line-light bg-about-mobile-light bg-contain bg-bottom bg-no-repeat dark:border-line-dark dark:bg-about-mobile-dark">
         <div className="grid grid-cols-2 gap-y-5 px-[16px] py-[50px]">
-          <p className="intersect:animate-fade-up col-span-2 text-center text-[24px] animate-delay-[100ms]">
+          <p className="col-span-2 text-center text-[24px] animate-delay-[100ms] intersect:animate-fade-up">
             JHONATHAN
           </p>
-          <p className="intersect:animate-fade-up col-span-2 text-right text-[24px] animate-delay-[200ms]">
+          <p className="col-span-2 text-right text-[24px] animate-delay-[200ms] intersect:animate-fade-up">
             FrontEnd
           </p>
-          <p className="intersect:animate-fade-up col-span-2 text-left text-[24px] animate-delay-[300ms]">
+          <p className="col-span-2 text-left text-[24px] animate-delay-[300ms] intersect:animate-fade-up">
             brazilian
           </p>
-          <p className="intersect:animate-fade-up col-span-2 text-right text-[24px] animate-delay-[400ms]">
+          <p className="col-span-2 text-right text-[24px] animate-delay-[400ms] intersect:animate-fade-up">
             English
           </p>
-          <p className="intersect:animate-fade-up col-span-2 text-left text-[24px] animate-delay-[500ms]">
+          <p className="col-span-2 text-left text-[24px] animate-delay-[500ms] intersect:animate-fade-up">
             dark mode
           </p>
-          <p className="intersect:animate-fade-up col-span-2 text-center text-[24px] animate-delay-[600ms]">
+          <p className="col-span-2 text-center text-[24px] animate-delay-[600ms] intersect:animate-fade-up">
             web applications
           </p>
-          <p className="intersect:animate-fade-up text-left text-[24px] animate-delay-[700ms]">
+          <p className="text-left text-[24px] animate-delay-[700ms] intersect:animate-fade-up">
             minimalist
           </p>
-          <p className="intersect:animate-fade-up text-right text-[24px] animate-delay-[800ms]">
+          <p className="text-right text-[24px] animate-delay-[800ms] intersect:animate-fade-up">
             code
           </p>
-          <p className="intersect:animate-fade-up col-span-2 text-center text-[24px] animate-delay-[900ms]">
+          <p className="col-span-2 text-center text-[24px] animate-delay-[900ms] intersect:animate-fade-up">
             React
           </p>
         </div>
