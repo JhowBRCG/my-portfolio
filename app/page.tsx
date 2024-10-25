90f56aa72d7897dab461884d2530bf5c7e5d46b1
Jhonathan <guarino90@gmail.com>
2024-10-25 20:12:21 -0300

    import Projects component inside page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
 import About from "@/components/About";
+import Projects from "@/components/Projects";
 
 export default function page() {
   return (
@@ -8,6 +9,7 @@ export default function page() {
       <Hero />
       <Skills />
       <About />
+      <Projects />
     </main>
   );
 }
