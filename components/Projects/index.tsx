ae2be6c4299db4c3fbd47db67eef82f73b0cdb0b
Jhonathan <guarino90@gmail.com>
2024-10-25 22:30:59 -0300

    add SubTitle component to Projects component

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,3 +1,9 @@
+import SubTitle from "../SubTitle";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <section className="mx-[22px] mt-[51px] xl:mx-[42px]">
+      <SubTitle text="Projects" textCenter={false} />
+    </section>
+  );
 }
