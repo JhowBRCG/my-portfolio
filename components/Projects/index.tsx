b756c356ae004c140a1559619cbd83992de3d4d7
Jhonathan <guarino90@gmail.com>
2024-10-29 21:38:01 -0300

    add grid wrapper <div> in Projects component and import ProjectCard

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,9 +1,16 @@
 import SubTitle from "../SubTitle";
+import { projects } from "@/data/projects";
+import ProjectCard from "../ProjectCard";
 
 export default function Projects() {
   return (
     <section className="mx-[22px] mt-[51px] xl:mx-[42px]">
       <SubTitle text="Projects" textCenter={false} />
+      <div className="mt-[25px] grid gap-2">
+        {projects.map((project, i) => (
+          <ProjectCard key={i} img={project.img} name={project.name} />
+        ))}
+      </div>
     </section>
   );
 }
