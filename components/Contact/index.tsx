7b4e34898be31fae22c54c2275cb4ad2d3668a3a
Jhonathan <guarino90@gmail.com>
2024-11-07 00:55:57 -0300

    add contact list, import icons from react-icons and, style Contact component

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,9 +1,37 @@
 import SubTitle from "../SubTitle";
+import { MdEmail } from "react-icons/md";
+import { FaLinkedinIn } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Contact() {
   return (
     <section className="mx-[22px] mt-[51px] xl:mx-[42px]">
       <SubTitle text="Contact with me" textCenter={true} />
+      <div className="mx-[-22px] mt-[46px] border-y border-line-light dark:border-line-dark xl:mx-[-42px]">
+        <ul className="flex items-center justify-between divide-x-2 divide-line-light dark:divide-line-dark">
+          <li className="w-full px-[23px] py-[20px]">
+            <a href="https://x.com/jhowbrcg">
+              <MdEmail className="mx-auto text-[27px]" />
+            </a>
+          </li>
+          <li className="w-full px-[23px] py-[20px]">
+            <a href="https://x.com/jhowbrcg">
+              <FaLinkedinIn className="mx-auto text-[27px]" />
+            </a>
+          </li>
+          <li className="w-full px-[23px] py-[20px]">
+            <a href="https://x.com/jhowbrcg">
+              <RiInstagramFill className="mx-auto text-[27px]" />
+            </a>
+          </li>
+          <li className="w-full px-[23px] py-[20px]">
+            <a href="https://x.com/jhowbrcg">
+              <FaXTwitter className="mx-auto text-[27px]" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
