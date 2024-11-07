56085fcd7d61c1b768206ea27cb24433ba6f4852
Jhonathan <guarino90@gmail.com>
2024-11-07 00:07:06 -0300

    add margin classes and import SubTitle component in Contact component

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,3 +1,9 @@
+import SubTitle from "../SubTitle";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="mx-[22px] mt-[51px] xl:mx-[42px]">
+      <SubTitle text="Contact with me" textCenter={true} />
+    </section>
+  );
 }
