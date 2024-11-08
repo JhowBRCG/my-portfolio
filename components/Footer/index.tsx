d63ef086af496e9f3b3836bdbfb520a2101fb4ed
Jhonathan <guarino90@gmail.com>
2024-11-07 22:25:19 -0300

    create Footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-[80px] pb-[39px] text-center xl:mt-[200px]">
+      &copy;2024 designed and coded by @jhowbrcg
+    </footer>
+  );
+}
