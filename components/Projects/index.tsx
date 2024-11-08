cb8adc929c63049727087d496a95d30a2056f118
Jhonathan <guarino90@gmail.com>
2024-11-07 22:48:36 -0300

    add media querie class to change margin-top in <section> tag in Projects component

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "../ProjectCard";
 
 export default function Projects() {
   return (
-    <section className="mx-[22px] mt-[51px] xl:mx-[42px]">
+    <section className="mx-[22px] mt-[51px] xl:mx-[42px] xl:mt-[102px]">
       <SubTitle text="Projects" textCenter={false} />
       <div className="mt-[25px] grid gap-2 md:grid-cols-2 md:gap-0 lg:grid-cols-3">
         {projects.map((project, i) => (
