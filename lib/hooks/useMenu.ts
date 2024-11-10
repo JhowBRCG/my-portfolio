fd331f2938e27f1a3af49b33eba8aebf128a9231
Jhonathan <guarino90@gmail.com>
2024-11-09 23:08:48 -0300

    toggle scroll overflow in useMenu hook based on menu state

--- lib/hooks/useMenu.ts
+++ lib/hooks/useMenu.ts
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useMenu() {
   const [menu, setMenu] = useState(false);
 
   const openMenu = () => setMenu(true);
   const closeMenu = () => setMenu(false);
+
+  useEffect(() => {
+    if (menu === true) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  });
 
   return { openMenu, closeMenu, menu };
 }
