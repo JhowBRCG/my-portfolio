d0801f69bd5ed1611d97cd402d62ca6c8fd685cd
Jhonathan <guarino90@gmail.com>
2024-11-10 21:12:30 -0300

    play audio on theme change in ToggleTheme

--- components/UI/ToggleTheme/index.tsx
+++ components/UI/ToggleTheme/index.tsx
@@ -10,14 +10,17 @@ type ToggleThemeProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 export default function ToggleTheme({ className }: ToggleThemeProps) {
   const isMounted = useIsMounted();
   const { theme, setTheme } = useTheme();
+  const soundEffect = new Audio("/Audios/toggle-theme.mp3");
+
+  const changeTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+    soundEffect.play();
+  };
 
   if (!isMounted) return null;
 
   return (
-    <button
-      className={cn(className)}
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-    >
+    <button className={cn(className)} onClick={changeTheme}>
       <Image
         src={`/images/switch-${theme === "light" ? "dark" : "light"}.svg`}
         width={"30"}
