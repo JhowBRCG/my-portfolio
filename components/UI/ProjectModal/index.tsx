dd54b8818f7b61728c4ce1a50050b38d20a28597
Jhonathan <guarino90@gmail.com>
2024-11-13 20:34:42 -0300

    add media query style classes to ProjectModal

--- components/UI/ProjectModal/index.tsx
+++ components/UI/ProjectModal/index.tsx
@@ -15,39 +15,39 @@ export default function ProjectModal({
   const { img, name, technologies, demo, repo } = project;
   return (
     <div className="fixed inset-0 h-full w-full bg-black-opacity">
-      <article className="absolute inset-0 m-auto h-fit w-[80%] bg-bg-light p-[20px] shadow-sm shadow-black-opacity dark:bg-bg-dark dark:shadow-white-opacity">
+      <article className="absolute inset-0 m-auto flex h-fit w-[80%] flex-col gap-[11px] bg-bg-light p-[20px] shadow-sm shadow-black-opacity dark:bg-bg-dark dark:shadow-white-opacity lg:flex-row">
         <Image
-          className="h-[150px] w-full object-cover object-top md:h-[200px]"
+          className="h-[150px] w-full object-cover object-top md:h-[200px] lg:h-[300px] lg:w-[50%] xl:h-[400px]"
           src={img}
           width={999}
           height={999}
           alt={name}
         />
-        <div>
-          <h3 className="mt-[11px] text-[24px]">{name}</h3>
+        <div className="flex-col lg:flex">
+          <h3 className="text-[20px] md:text-[24px] xl:text-[28px]">{name}</h3>
           <ul className="flex gap-[10px]">
             {technologies.map((tech, i) => (
-              <li className="mt-[10px] text-[22px]" key={i}>
+              <li className="mt-[10px] text-[22px] xl:text-[30px]" key={i}>
                 {tech}
               </li>
             ))}
           </ul>
-          <p className="mt-[10px] text-justify">
+          <p className="mt-[10px] text-justify xl:mt-[20px]">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
             laudantium hic! Vero facere id, inventore nulla quasi magni nemo
             minus
           </p>
-          <div className="mb-[50px] mt-[21px] flex justify-between gap-[3px]">
+          <div className="mb-[50px] mt-[21px] flex justify-between gap-[3px] lg:mb-0 lg:mt-auto lg:justify-start">
             <Link
               target="_blank"
-              className="w-full border border-line-light p-[7px] text-center dark:border-line-dark"
+              className="w-full border border-line-light p-[7px] text-center dark:border-line-dark lg:w-[150px] xl:w-[200px]"
               href={demo}
             >
               VIEW PAGE
             </Link>
             <Link
               target="_blank"
-              className="w-full border border-line-light p-[7px] text-center dark:border-line-dark"
+              className="w-full border border-line-light p-[7px] text-center dark:border-line-dark lg:w-[150px] xl:w-[200px]"
               href={repo}
             >
               RESPOSITORY
