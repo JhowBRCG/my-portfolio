b2a33717185a4768ec4a1c4da338d19be5821a60
Jhonathan <guarino90@gmail.com>
2024-11-13 18:38:05 -0300

    add <div> wrapper in ProjectModal

--- components/UI/ProjectModal/index.tsx
+++ components/UI/ProjectModal/index.tsx
@@ -23,40 +23,43 @@ export default function ProjectModal({
           height={999}
           alt={name}
         />
-        <h3 className="mt-[11px] text-[24px]">{name}</h3>
-        <ul className="flex gap-[10px]">
-          {technologies.map((tech, i) => (
-            <li className="mt-[10px] text-[22px]" key={i}>
-              {tech}
-            </li>
-          ))}
-        </ul>
-        <p className="mt-[10px] text-justify">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
-          laudantium hic! Vero facere id, inventore nulla quasi magni nemo minus
-        </p>
-        <div className="mb-[50px] mt-[21px] flex justify-between gap-[3px]">
-          <Link
-            target="_blank"
-            className="w-full border border-line-light p-[7px] text-center dark:border-line-dark"
-            href={demo}
+        <div>
+          <h3 className="mt-[11px] text-[24px]">{name}</h3>
+          <ul className="flex gap-[10px]">
+            {technologies.map((tech, i) => (
+              <li className="mt-[10px] text-[22px]" key={i}>
+                {tech}
+              </li>
+            ))}
+          </ul>
+          <p className="mt-[10px] text-justify">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum,
+            laudantium hic! Vero facere id, inventore nulla quasi magni nemo
+            minus
+          </p>
+          <div className="mb-[50px] mt-[21px] flex justify-between gap-[3px]">
+            <Link
+              target="_blank"
+              className="w-full border border-line-light p-[7px] text-center dark:border-line-dark"
+              href={demo}
+            >
+              VIEW PAGE
+            </Link>
+            <Link
+              target="_blank"
+              className="w-full border border-line-light p-[7px] text-center dark:border-line-dark"
+              href={repo}
+            >
+              RESPOSITORY
+            </Link>
+          </div>
+          <button
+            onClick={closeModal}
+            className="absolute bottom-0 right-0 border-l border-t border-line-light p-[10px] text-[20px] dark:border-line-dark"
           >
-            VIEW PAGE
-          </Link>
-          <Link
-            target="_blank"
-            className="w-full border border-line-light p-[7px] text-center dark:border-line-dark"
-            href={repo}
-          >
-            RESPOSITORY
-          </Link>
+            <MdOutlineClose />
+          </button>
         </div>
-        <button
-          onClick={closeModal}
-          className="absolute bottom-0 right-0 border-l border-t border-line-light p-[10px] text-[20px] dark:border-line-dark"
-        >
-          <MdOutlineClose />
-        </button>
       </article>
     </div>
   );
