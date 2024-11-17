b7179385cb5827850d75741e3da0962416f33962
Jhonathan <guarino90@gmail.com>
2024-11-17 19:47:17 -0300

    refactor useModal hook

--- lib/hooks/useModal.ts
+++ lib/hooks/useModal.ts
@@ -3,13 +3,9 @@ import { useState } from "react";
 export default function useModal() {
   const [modal, setModal] = useState(false);
 
-  const openModal = () => {
-    setModal(true);
-  };
+  const openModal = () => setModal(true);
 
-  const closeModal = () => {
-    setModal(false);
-  };
+  const closeModal = () => setModal(false);
 
   return { modal, openModal, closeModal };
 }
